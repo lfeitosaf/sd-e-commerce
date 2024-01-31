8560fe9fd80118ec83e86908764d376eb87689a6
Lucas <lucas-jpl@hotmail.com>
2024-01-30 23:22:21 -0300

    add colors extension

--- sd-e-commerce/tailwind.config.ts
+++ sd-e-commerce/tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      "light-green": "#00ff9f",
+      "light-blue": "#00b8ff",
+      "dark-blue": "#001eff",
+      "dark-violet": "#bd00ff",
+      "dark-pink": "#d600ff",
     },
   },
   plugins: [],
